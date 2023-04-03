e9ed31adc0491582853a75b5fdb2ee53c94937e3
Deus Vult <clarencejoven1456@gmail.com>
2023-04-03 19:24:04 +0800

    Made the destructibles work.
    Now destructible objects can be destroyed but only those affected by the explosion does.

--- الله اكبر/Assets/Scripts/Explosion.cs
+++ الله اكبر/Assets/Scripts/Explosion.cs
@@ -8,7 +8,7 @@ public class Explosion : MonoBehaviour
     {
         if (explosion.gameObject.tag == "Destructible") 
         {
-            
+            Destroy(explosion.gameObject);
         }
         if (explosion.gameObject.name == "BumbirBlue") 
         {
